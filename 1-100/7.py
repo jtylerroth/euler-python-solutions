bd2d6459e2aa09576be51e2f6dc03cbda665a142
Will Eddins <eddinsw@gmail.com>
2016-03-30 01:15:17 -0400

    7 Solved

---
+++ 1-100/7.py
@@ -0,0 +1,30 @@
+# By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+#
+# What is the 10 001st prime number?
+
+def getPrimes():
+	# Stolen from Euler #3, but properly yields the first primes
+	primes = [2,3]
+	num = 3
+
+	yield 2
+	yield 3
+
+	while True:
+		num += 2
+		isPrime = True
+		for prime in primes:
+			if num % prime == 0:
+				isPrime = False
+				break
+
+		if isPrime:
+			primes.append(num)
+			yield num
+
+primes = getPrimes()
+
+for i in range(0,10000):
+	next(primes)
+
+print(next(primes))
